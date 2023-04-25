58b3891eb6da4db78d2ecc2c4cd3c495391322c1
djketchum <dketchum@ucsd.edu>
2023-04-24 21:20:26 -0700

    h

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -16,25 +16,27 @@ public class ArrayExamples {
     for(int i = 0; i < arr.length; i += 1) {
       arr[i] = newArray[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
   // lowest number when calculating. Returns 0 if there are no elements or just
   // 1 element in the array
   static double averageWithoutLowest(double[] arr) {
+    int i = 0;
     if(arr.length < 2) { return 0.0; }
     double lowest = arr[0];
     for(double num: arr) {
       if(num < lowest) { lowest = num; }
     }
+    for(int j = 0; j<arr.length;j++) {
+      if(arr[j] == lowest) {i +=1;}
+    }
     double sum = 0;
     for(double num: arr) {
       if(num != lowest) { sum += num; }
     }
-    return sum / (arr.length - 1);
+    return sum / (arr.length - i);
   }
-
-
 }
 
--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,30 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedd() {
+    int[] input = {2,3,4,70};
+    assertArrayEquals(new int[] {70,4,3,2}, ArrayExamples.reversed(input));
+  }
+
+  @Test
+  public void testReverseInPlacee() {
+    int[] input2 = {2,3,4,7};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[] {7,4,3,2}, input2);
+  }
+
+  @Test
+  public void testAvgWithoutLowest() {
+    double[] inputt = {6.0,8.0,2.0,2.0,4.0};
+    assertEquals(6.0,ArrayExamples.averageWithoutLowest(inputt),1.0);
+  }
+
+  @Test
+  public void testAvgWithoutLowestt() {
+    double[] input = {6.0,2.0,7.0};
+    assertEquals(6.5,ArrayExamples.averageWithoutLowest(input),1.0);
+  }
+
 }
